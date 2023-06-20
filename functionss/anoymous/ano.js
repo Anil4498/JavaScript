82a91d3a868fa764b2b7b888cf316a3a6b526a4b
SaILU4498 <anilrathod19977@gmail.com>
2023-06-20 14:40:07 +0530

    learnt

---
+++ functionss/anoymous/ano.js
@@ -0,0 +1,9 @@
+var myfun = function () {
+    return "1234";
+};
+console.log(myfun());
+// or
+var display = function () {
+    console.log("1234");
+};
+display();
---
+++ functionss/anoymous/ano.ts
@@ -0,0 +1,12 @@
+var myfun=function(){
+
+    return "1234"
+}
+console.log(myfun())
+
+// or
+
+var display =function(){
+    console.log("1234")
+}
+display()
